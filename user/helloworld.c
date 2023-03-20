05eef62dc580a8665409a09211b5c4e0f313d088
Batel Shkolnik <batelshk@post.bgu.ac.il>
2023-03-20 15:29:09 +0200

    Task 1 and 2

---
+++ user/helloworld.c
@@ -0,0 +1,9 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, int *argv[]){
+    write(1, "Hello World xv6", 15);
+
+    exit(0);
+}
---
+++ user/memsize_test.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+    int bytes_of_memory = memsize();
+    printf("%d\n", bytes_of_memory);
+
+
+    void * mac = malloc(20000*sizeof(char));
+    bytes_of_memory = memsize();
+    printf("%d\n", bytes_of_memory);
+   
+
+    free(mac);
+    bytes_of_memory = memsize();
+    printf("%d\n", bytes_of_memory);
+
+    exit(0);
+}
